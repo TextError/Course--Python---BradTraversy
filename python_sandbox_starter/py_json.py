91878dcd4f7bdd9e4dca592c0954b6f22be37391
TextError <savinraul1983@yahoo.com>
2019-01-04 10:00:21 +0000

    JSON

--- python_sandbox_starter/py_json.py
+++ python_sandbox_starter/py_json.py
@@ -1,1 +1,16 @@
 # JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+
+import json
+
+# Sample JSON
+userJson = '{"first_name": "John", "last_name": "Doe", "age": 30}'
+
+# Parse to dict
+user = json.loads(userJson)
+print(user)
+print(user['first_name'])
+
+# Dict parse to JSON
+car = {'make': 'Ford', 'model': 'Mustang', 'year': 1970}
+carJSON = json.dumps(car)
+print(carJSON)
